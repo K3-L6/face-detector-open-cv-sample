6ee399f2f00c4f4d86ae171a2fa4bb4ee5d6dc96
K3 L6 <kennlising23@gmail.com>
2021-01-27 12:08:28 +0800

    Multiple face detection

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-colored_img = cv2.imread('RDJ.jpg')
+colored_img = cv2.imread('RDJ-JD.jpg')
 
 grayscaled_img = cv2.cvtColor(colored_img, cv2.COLOR_BGR2GRAY)
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
